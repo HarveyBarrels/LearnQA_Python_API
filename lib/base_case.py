03480aa2c8c3ac7668d6cc5f35649cc0ee46ff64
HarveyBarrels <evil0013@yandex.ru>
2021-09-22 22:03:57 +0300

    Exercise 17: Negative PUT tests: User editing

--- lib/base_case.py
+++ lib/base_case.py
@@ -2,6 +2,7 @@ import json.decoder
 from requests import Response
 from datetime import  datetime
 
+
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"There's no cookie with name {cookie_name} in the last response"
