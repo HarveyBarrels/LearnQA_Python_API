fb8cf2bff1533e8a8b15396c231e898d875e99e9
HarveyBarrels <evil0013@yandex.ru>
2021-09-26 16:19:23 +0300

    Exercise 18: DELETE tests and Exercise 19: Allure tags

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -4,8 +4,20 @@ from lib.assertions import Assertions
 import pytest
 import random
 import string
+import  allure
 
+#Random string data
+letters = string.ascii_lowercase
+rand_string = ''.join(random.choice(letters) for i in range(251))
+
+@allure.epic("User registration cases")
 class TestUserRegister(BaseCase):
+    incorrect_data_set = [
+        ({'email': 'vinkotovexample.com'}, ("Invalid email format")),
+        ({'firstName': "c"}, ("The value of 'firstName' field is too short")),
+        ({'firstName': rand_string}, ("The value of 'firstName' field is too long"))
+    ]
+
     excluded_params = [
         ("email"),
         ("password"),
@@ -14,50 +26,43 @@ class TestUserRegister(BaseCase):
         ("username")
     ]
 
+    @allure.description("This test checks successful user registration using email and password"
+                        "and filling in 'firstName', 'lastName', and 'username' fields")
     def test_create_new_user_succesfully(self):
         my_data = self.prepare_registration_data()
-        response = MyRequests.post("/user/", data = my_data)
+        with allure.step("Register new generated user"):
+            response = MyRequests.post("/user/", data = my_data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         my_email = 'vinkotov@example.com'
         my_data = self.prepare_registration_data(my_email)
-        response = MyRequests.post("/user", data = my_data)
+        with allure.step(f"Try to register existing user with email={my_email}"):
+            response = MyRequests.post("/user", data = my_data)
 
         Assertions.assert_code_status(response, 400)
         Assertions.assert_expected_response_content(response, f"Users with email '{my_email}' already exists")
 
-
-    def test_create_user_with_incorrect_email(self):
-        incorrect_email = 'vinkotovexample.com'
-        my_data = self.prepare_registration_data(incorrect_email)
-        response = MyRequests.post("/user/", data = my_data)
-        Assertions.assert_code_status(response, 400)
-        Assertions.assert_expected_response_content(response, "Invalid email format")
-
-    def test_create_user_with_too_short_name(self):
+    @allure.description("This test checks unsuccessful user registration"
+                        "using email with incorrect format, too short or too long first name of user")
+    @pytest.mark.parametrize("wrong_data, expected_error_message", incorrect_data_set)
+    def test_create_user_with_incorrect_data(self, wrong_data, expected_error_message):
         my_data = self.prepare_registration_data()
-        my_data["username"]="q"
-        response=MyRequests.post("/user", data=my_data)
+        my_data.update(wrong_data)
+        with allure.step(f"Try to register new user with incorrect data = {my_data}"):
+            response=MyRequests.post("/user", data=my_data)
         Assertions.assert_code_status(response, 400)
-        Assertions.assert_expected_response_content(response, "The value of 'username' field is too short")
+        Assertions.assert_expected_response_content(response, expected_error_message)
 
-    def test_create_user_with_too_long_name(self):
-        #сгенерировать случайную строку
-        letters = string.ascii_lowercase
-        rand_string = ''.join(random.choice(letters) for i in range(251))
-        my_data = self.prepare_registration_data()
-        my_data["username"]=rand_string
-        response=MyRequests.post("/user", data=my_data)
-        Assertions.assert_code_status(response, 400)
-        Assertions.assert_expected_response_content(response, "The value of 'username' field is too long")
-
+    @allure.description("This test checks unsuccessful user registration"
+                        " with any of required user data fields missed")
     @pytest.mark.parametrize('missed_field', excluded_params)
     def test_negative_register_check(self, missed_field):
         my_data = self.prepare_registration_data()
         del my_data[f"{missed_field}"]
-        response = MyRequests.post("/user", data=my_data)
+        with allure.step(f"Try to register new user with data field '{missed_field}' missing"):
+            response = MyRequests.post("/user", data=my_data)
 
         Assertions.assert_code_status(response, 400)
         Assertions.assert_expected_response_content(response, f"The following required params are missed: {missed_field}" )
